4ed1c0678d6b140f8914bc608eb3b53f4fd2d4af
Curtis Shmyr <cjshmyr@gmail.com>
2017-05-29 23:14:32 -0600

    More WIP

--- src/AStar.ts
+++ src/AStar.ts
@@ -6,7 +6,7 @@
 
 class ANode {
     position: Point;
-    // parent: ANode;
+    parent: ANode;
     f: number = 0; // heuristic cost estimate (g+h)
     g: number = 0; // cost (g)
     readonly isWalkable: boolean;
@@ -32,7 +32,7 @@ class AStar {
         let start = new Point(0, 0);
         let goal = new Point(3, 4);
 
-        let nodes = [];
+        let nodes: ANode[][] = [];
         for (let y = 0; y < map.length; y++) {
             nodes[y] = [];
             for (let x = 0; x < map[y].length; x++) {
@@ -43,70 +43,118 @@ class AStar {
         let open: ANode[] = [];
         let closed: ANode[] = [];
 
-        let path: ANode[] = []; // Final path.
-
         let origin = new ANode(start.x, start.y, true);
         origin.g = 0;
         origin.f = origin.g + this.heuristicManhattan(start, goal); // http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html#S7;
         open.push(origin);
 
         while (open.length != 0) {
+            console.log('open count:' + open.length);
             let node = this.popLowestScoreNode(open);
+
+            console.log('lowest score node:' + node.position.x + ',' + node.position.y);
 
             if (node.position.equals(goal)) {
                 // done; break
+                console.log('path made');
+                this.reconstructPath(nodes, node);
                 break;
             }
 
-            open.splice(open.indexOf(node), 1);
             closed.push(node);
 
             let adjacentPoints = this.getAdjacent(node, width, height);
+
             for (let p of adjacentPoints) {
                 let next: ANode = nodes[p.y][p.x];
-                // next.parent = node;
 
-                /* this part added by me */
+                console.log('- neighbour:' + p.x + ',' + p.y);
+
+                /* this part below by me */
                 if (!next.isWalkable) { // Isn't walkable (TODO: combine with closed check. potentially we can just deal with one array, with undefined/open/closed states)
-                    closed.push(node);
+                    // closed.push(node);
+                    console.log('-- not walkable; done');
                     continue;
                 }
-                /* this part added by me */
+                /* this part above by me */
 
                 if (closed.indexOf(next) > -1) { // Is closed
+                    console.log('-- is closed; done');
                     continue;
                 }
 
                 let tentativeG = node.g + Point.distance(node.position, next.position);
 
                 if (open.indexOf(next) == -1) { // Add if not in open set
+                    if (next.position.x == 1 && next.position.y == 4) {
+                        let anotherdebug = true;
+                    }
+                    console.log('-- not open; adding to open')
                     open.push(next);
                 }
                 else if (tentativeG >= next.g) { // Already open...
+                    console.log('-- already open, poor score; done');
                     continue; // but wasn't a better path
                 }
 
                 // Best path until now
+                console.log('-- BEST');
+                if (next.position.x == 2 && next.position.y == 2) {
+                    let debug = true;
+                }
+
                 next.g = tentativeG;
                 next.f = next.g + this.heuristicManhattan(next.position, goal);
-                path.push(node);
+                next.parent = node;
             }
 
-            closed.push(node);
+            // closed.push(node);
         }
+
+        // TODO: Return failure
     }
 
-    popLowestScoreNode(nodes: ANode[]) : ANode {
+    // TODO: This doesn't sort our path the way we want it, nor does it include start/end yet.
+    reconstructPath(nodes: ANode[][], current: ANode) : ANode[] {
+        let path = [];
+
+        while (true) {
+            let parent = this.gerParent(current, nodes);
+
+            if (parent == null) {
+                break;
+            }
+            else {
+                path.push(parent);
+                current = parent;
+            }
+        }
+
+        return path;
+    }
+
+    gerParent(child: ANode, nodes: ANode[][]) : ANode {
+        for (let y = 0; y < nodes.length; y++) {
+            for (let x = 0; x < nodes[y].length; x++) {
+                if (nodes[y][x] == child.parent) {
+                    return child.parent;
+                }
+            }
+        }
+        return null;
+    }
+
+    popLowestScoreNode(open: ANode[]) : ANode {
         let bestIndex = 0;
 
-        for (let i = 0; i < nodes.length; i++) {
-            if (nodes[i].f < nodes[bestIndex].f) {
+        for (let i = 0; i < open.length; i++) {
+            if (open[i].f < open[bestIndex].f) {
                 bestIndex = i;
             }
         }
 
-        let bestNode = nodes[bestIndex];
-        nodes.splice(bestIndex, 1);
+        let bestNode = open[bestIndex];
+        open.splice(bestIndex, 1);
         return bestNode;
     }
 
@@ -136,7 +184,6 @@ class AStar {
 
         return neighbours;
     }
-
 
     heuristicManhattan(node: Point, goal: Point) : number { // Good for 4 directions
         let D = 1;
